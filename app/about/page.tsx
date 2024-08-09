7a94fee40688ddab6c97ed9ad09c6b767f42232b
James Wallington <jameswallington@Jamess-MacBook-Pro.local>
2024-08-09 18:15:11 +0100

    basic layout and file structure

---
+++ app/about/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        euismod libero, id lacinia nunc. Nullam auctor, nunc id aliquet
+        ullamcorper, nisl nunc tincidunt nunc, vitae lacinia nunc nunc vitae
+        mauris. Sed nec nunc auctor, aliquet nunc id, aliquam nunc. Sed
+        vestibulum, nunc nec tincidunt feugiat, nunc nunc tincidunt nunc, vitae
+        lacinia nunc nunc vitae mauris. Sed nec nunc auctor, aliquet nunc id,
+        aliquam nunc. Sed vestibulum, nunc nec tincidunt feugiat, nunc nunc
+        tincidunt nunc, vitae lacinia nunc nunc vitae mauris.
+      </p>
+    </div>
+  );
+};
+
+export default About;
